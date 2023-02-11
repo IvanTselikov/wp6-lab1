be53acfc5b67a219578961b08267b26e67a4ce79
IvanTselikov <ivantselikov@yandex.ru>
2023-02-12 00:56:21 +0300

    Добавлена возможность показать/скрыть пароль

---
+++ login.php
@@ -0,0 +1,179 @@
+<?php
+// session_start();
+/* TODO:
+Адаптивность
+именование
+семантическая вёртска
+рефакторинг
+тестирование
+учитывать ограничения из бд
+не учитывать в regex концевые пробелы
+другой выпадающий список
+оформление формы bootstrap
+bootstrap валидация
+стили в css
+в js табуляция 4 символа
+*/
+?>
+
+<!doctype html>
+<html lang="ru">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <title>PHP Авторизация/Регистрация</title>
+</head>
+
+<body>
+    <header class="d-flex justify-content-end">
+        <button id="theme-switcher" class="btn btn-primary m-3" data-theme="light">
+            <i class="fa fa-sun-o me-1" aria-hidden="true"></i>
+            <span>Светлая тема</span>
+        </button>
+    </header>
+    <main>
+        <div id="login-tab-control" class="w-50 mx-auto mb-5 px-5 py-4 border"
+            style="border-radius: 0.75rem; background-color: rgb(245, 245, 245);">
+            <h3 class="text-center mb-5 mt-4">Добро пожаловать!</h3>
+            <ul class="nav nav-tabs" id="myTab" role="tablist">
+                <li class="nav-item" role="presentation">
+                    <button class="nav-link active" id="author-tab-btn" data-bs-toggle="tab"
+                        data-bs-target="#author-tab-content" type="button" role="tab" aria-controls="author-tab-content"
+                        aria-selected="true">Войти</button>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <button class="nav-link" id="registr-tab-btn" data-bs-toggle="tab"
+                        data-bs-target="#registr-tab-content" type="button" role="tab"
+                        aria-controls="registr-tab-content" aria-selected="false">Зарегистрироваться</button>
+                </li>
+            </ul>
+            <div class="tab-content" id="main-tab-panel">
+                <div class="tab-pane fade show active" id="author-tab-content" role="tabpanel"
+                    aria-labelledby="author-tab-btn">
+                    <form id="author-form">
+                        <div class="mt-4 mb-3">
+                            <label for="author-input-login" class="col-form-label">Логин</label>
+                            <input type="text" class="form-control" id="author-input-login" autocomplete="off" required>
+                        </div>
+
+                        <div class="mb-4">
+                            <label for="author-input-password" class="col-form-label">Пароль</label>
+                            <div class="password-group input-group mb-3">
+                                <input type="password" class="form-control" id="author-input-password"
+                                    autocomplete="off" aria-describedby="author-show-password" required>
+                                <button class="btn btn-primary" type="button" id="author-show-password">
+                                    <i class="fa fa-eye-slash" aria-hidden="true"></i>
+                                </button>
+                            </div>
+                        </div>
+
+                        <div class="d-grid gap-2">
+                            <button type="submit" form="author-form"
+                                class="btn btn-primary btn-block my-2">Войти</button>
+                        </div>
+                    </form>
+                </div>
+                <div class="tab-pane fade" id="registr-tab-content" role="tabpanel" aria-labelledby="registr-tab-btn">
+                    <form id="registr-form">
+                        <div class="mt-4 mb-3">
+                            <label for="registr-input-first-name" class="col-form-label">Имя</label>
+                            <input type="text" class="form-control" id="registr-input-first-name"
+                                pattern="[a-zA-Zа-яА-ЯЁё\s'-]{2,15}" autocomplete="off" required>
+                            <div class="form-text">только текст, не менее 2 символов, не более 15</div>
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="registr-input-second-name" class="col-form-label">Фамилия</label>
+                            <input type="text" class="form-control" id="registr-input-second-name"
+                                pattern="[a-zA-Zа-яА-ЯЁё\s'-]{2,15}" autocomplete="off" required>
+                            <div class="form-text">только текст, не менее 2 символов, не более 15</div>
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="registr-input-email" class="col-form-label">Email</label>
+                            <input type="text" class="form-control" id="registr-input-email"
+                                pattern="[a-zA-Z0-9]+@[a-zA-Z]+\.[a-z]+" placeholder="username@service.com"
+                                title="username@service.com" autocomplete="off" required>
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="registr-input-login" class="col-form-label">Логин</label>
+                            <input type="text" class="form-control" id="registr-input-login" pattern="[^\s]{6,}"
+                                autocomplete="off" required>
+                            <div class="form-text">не менее 6 символов</div>
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="registr-input-password" class="col-form-label">Придумайте пароль</label>
+                            <div class="password-group input-group mb-3">
+                                <input type="password" class="form-control" id="registr-input-password"
+                                    aria-describedby="registr-show-password"
+                                    pattern="/(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*_=+]).{8,}/gm"
+                                    autocomplete="off" required>
+                                <button class="btn btn-primary" type="button" id="registr-show-password">
+                                    <i class="fa fa-eye-slash" aria-hidden="true"></i>
+                                </button>
+                            </div>
+                            <div class="form-text">не менее 8 символов, обязательно совместное использование
+                                прописных, строчных букв, цифр, символов !@#$%^&*_=+</div>
+                        </div>
+
+                        <div class="mb-4">
+                            <label for="registr-input-password-check" class="col-form-label">Повторите
+                                пароль</label>
+                            <div class="password-group input-group mb-3">
+                                <input type="password" class="form-control" id="registr-input-password-check"
+                                    aria-describedby="registr-show-password-check" autocomplete="off" required>
+                                <button class="btn btn-primary" type="button" id="registr-show-password-check">
+                                    <i class="fa fa-eye-slash" aria-hidden="true"></i>
+                                </button>
+                            </div>
+                        </div>
+
+                        <div class="mb-4">
+                            <p class="mb-2">Пол</p>
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input" type="radio" id="male" name="gender" value="male"
+                                    required>
+                                <label class="form-check-label" for="male">Мужской</label>
+                            </div>
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input" type="radio" id="female" name="gender" value="female">
+                                <label class="form-check-label" for="female">Женский</label>
+                            </div>
+                        </div>
+
+                        <select class="mb-4 form-select" name="is-adult">
+                            <option selected value="not-adult">Мне нет 18 лет</option>
+                            <option value="adult">Мне исполнилось 18 лет</option>
+                        </select>
+
+                        <div class="mb-4 form-check">
+                            <input type="checkbox" class="form-check-input" name="rules" required>
+                            <label class="form-check-label" for="rules">Принимаю Пользовательское соглашение</label>
+                        </div>
+
+                        <div class="d-grid gap-2">
+                            <button type="submit" form="registr-form" class="btn btn-primary my-3">Регистрация</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </main>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4"
+        crossorigin="anonymous"></script>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
+    <script type="text/javascript" src="script.js"></script>
+</body>
+
+</html>
--- script.js
+++ script.js
@@ -1,36 +1,56 @@
 $(document).ready(function () {
-  $('#theme-switcher').click(function () {
-    // TODO: data- атрибут
-    if ($(this).text() === 'Светлая тема') {
-      $(this).text('Тёмная тема')
+  // смена темы
+  $('#theme-switcher').click(function () {    
+    if ($(this).data('theme') === 'light') {
+      $(this).data('theme', 'dark')
+
+      $(this).addClass('btn-secondary')
+      $(this).find('i').addClass('fa-moon-o').removeClass('fa-sun-o')
+      $(this).find('span').text('Тёмная тема')
+
       $('body').addClass('text-white')
       $('body').addClass('bg-dark')
+
       $('#login-tab-control').addClass('bg-secondary')
       $('#login-tab-control').addClass('border-secondary')
-      $(this).addClass('btn-secondary')
-      $('button[type="submit"]').addClass('btn-dark')
       $('button.nav-link').addClass('tabs-dark')
+
       $('.form-text').addClass('tips-dark')
-      // $('body').addClass('bg-secondary')
-      // $('#login-tab-control').addClass('bg-dark')
-      // $('#login-tab-control').addClass('border-dark')
-      // $(this).addClass('btn-dark')
+      $('.password-group button').addClass('btn-dark')
+      $('button[type="submit"]').addClass('btn-dark')
     }
     else {
-      $(this).text('Светлая тема')
+      $(this).data('theme', 'light')
+
+      $(this).removeClass('btn-secondary')
+      $(this).find('i').addClass('fa-sun-o').removeClass('fa-moon-o')
+      $(this).find('span').text('Светлая тема')
+
       $('body').removeClass('text-white')
       $('body').removeClass('bg-dark')
+
       $('#login-tab-control').removeClass('bg-secondary')
       $('#login-tab-control').removeClass('border-secondary')
-      $(this).removeClass('btn-secondary')
-      $('button[type="submit"]').removeClass('btn-dark')
       $('button.nav-link').removeClass('tabs-dark')
-      $('.form-text').removeClass('tips-dark')
-      // $('body').removeClass('bg-secondary')
-      // $('#login-tab-control').removeClass('bg-dark')
-      // $('#login-tab-control').removeClass('border-dark')
-      // $(this).removeClass('btn-dark')
-    }
 
+      $('.form-text').removeClass('tips-dark')
+      $('.password-group button').removeClass('btn-dark')
+      $('button[type="submit"]').removeClass('btn-dark')
+    }
+  })
+
+  // показать/скрыть пароль
+  $('.password-group button').click(function() {
+    let input = $(this).parent().find('input')
+    let eye = $(this).find('i')
+    if (input.attr('type') === 'text') {
+      input.attr('type', 'password')
+      eye.addClass('fa-eye-slash')
+      eye.removeClass('fa-eye')
+    } else {
+      input.attr('type', 'text')
+      eye.removeClass('fa-eye-slash')
+      eye.addClass('fa-eye')
+    }
   })
 })
